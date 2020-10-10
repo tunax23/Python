e5e936ad63efd30903a45ddec1e194d3d4cbd7e0
Tunax-C <ariestina.tc@gmail.com>
2020-10-10 14:12:26 +0800

    Create def.py

---
+++ def.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  def.py
+#  
+#  Copyright 2020 Tina Chen <ChenTina@10-72-120-211.wifi.8.s.pas.ofs.edu.sg>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+
+def main(args):
+    return 0
+
+if main == '':
+    import sys
+    sys.exit(main(sys.argv))
+
+def A():
+	print("Ananya")
+def B():
+	print("buys")
+def C():
+	print("Chocolates")
+
+A()
+B()
+C()	
+exit()
